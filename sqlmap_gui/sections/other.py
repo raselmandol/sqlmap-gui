6fa4b69c163d2f73448cb2408257ab4dbd0892b8
Md Rasel Mandol <raselmandol@yahoo.com>
2025-01-25 21:12:15 +0600

    removed widget contents from other tab

--- sqlmap_gui/sections/other.py
+++ sqlmap_gui/sections/other.py
@@ -9,36 +9,36 @@ class OtherTab(QWidget):
     def initUI(self):
 
         self.layout = QVBoxLayout()
-        self.verbose = QCheckBox("--verbose")
-        self.layout.addWidget(self.verbose)
+        # self.verbose = QCheckBox("--verbose")
+        # self.layout.addWidget(self.verbose)
 
         # self.threads = QLineEdit(self)
         # self.threads.setPlaceholderText("--threads")
         # # self.layout.addWidget(QLabel("--threads"))
         # self.layout.addWidget(self.threads)
 
-        self.level = QLineEdit(self)
-        self.level.setPlaceholderText("--level")
-        # self.layout.addWidget(QLabel("--level"))
-        self.layout.addWidget(self.level)
+        # self.level = QLineEdit(self)
+        # self.level.setPlaceholderText("--level")
+        # # self.layout.addWidget(QLabel("--level"))
+        # self.layout.addWidget(self.level)
 
         self.setLayout(self.layout)
 
     def collectInputs(self):
         inputs = []
 
-        if self.verbose.isChecked():
-            inputs.append("--verbose")
+        # if self.verbose.isChecked():
+        #     inputs.append("--verbose")
 
-        # if self.threads.text():
-        #     inputs.append(f"--threads={self.threads.text()}")
+        # # if self.threads.text():
+        # #     inputs.append(f"--threads={self.threads.text()}")
 
-        if self.level.text():
-            inputs.append(f"--level={self.level.text()}")
+        # if self.level.text():
+        #     inputs.append(f"--level={self.level.text()}")
 
         return inputs
 
     def clearInputs(self):
-        self.verbose.setChecked(False)
-        # self.threads.clear()
-        self.level.clear()
+        # self.verbose.setChecked(False)
+        # # self.threads.clear()
+        # self.level.clear()
