cda460ce8d17557ffa0077faae84ccf70c920366
raselmandol <raselmandol@yahoo.com>
2025-06-11 21:55:13 +0600

    added detection_tab contents

--- sqlmap_gui/sections/detection.py
+++ sqlmap_gui/sections/detection.py
@@ -8,19 +8,47 @@ class DetectionTab(QWidget):
     def initUI(self):
         self.layout = QVBoxLayout()
 
+        self.dlevel = QComboBox()
+        self.dlevel.addItem("Select")
+        self.dlevel.addItems(["1", "2", "3", "4", "5"])
+        self.layout.addWidget(QLabel("--level"))
+        self.layout.addWidget(self.dlevel)
+
+        self.risk = QComboBox()
+        self.risk.addItem("Select")
+        self.risk.addItems(["1", "2", "3"])
+        self.layout.addWidget(QLabel("--risk"))
+        self.layout.addWidget(self.risk)
+
+        self.dstring = QLineEdit(self)
+        self.dstring.setPlaceholderText("e.g., STRING")
+        self.layout.addWidget(QLabel("--string"))
+        self.layout.addWidget(self.dstring)
+
+
+        self.notString = QLineEdit(self)
+        self.notString.setPlaceholderText("e.g., NOT-STRING")
+        self.layout.addWidget(QLabel("--not-string"))
+        self.layout.addWidget(self.notString)
+
        
         self.setLayout(self.layout)
 
-    # def collectInputs(self):
-    #     inputs = []
-
+    def collectInputs(self):
         
+        inputs = []
 
-    #     return inputs
+        dlevel_value = self.dlevel.currentText()
+        if dlevel_value and dlevel_value!="Select":
+            inputs.append(f"--level={dlevel_value}")
 
-    # def clearInputs(self):
-    #     self.technique.clear()
-    #     self.time_sec.clear()
-    #     self.union_cols.clear()
-    #     self.union_char.clear()
-    #     self.no_cast.setChecked(False)
+        risk_value = self.risk.currentText()
+        if risk_value and risk_value!="Select":
+            inputs.append(f"--risk={risk_value}")
+
+
+        return inputs
+
+    def clearInputs(self):
+        self.dlevel.setCurrentIndex(0)
+        self.risk.setCurrentIndex(0)       
