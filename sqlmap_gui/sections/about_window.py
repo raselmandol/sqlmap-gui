0b37e0589fea83a13cc67df165fbc2ffc29bb87a
raselmandol <raselmandol@yahoo.com>
2025-07-09 23:37:19 +0600

    add: about_window template

---
+++ sqlmap_gui/sections/about_window.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTextEdit
+from PyQt5.QtCore import Qt
+
+class AboutWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About - sqlmap-GUI")
+        self.setGeometry(300, 200, 600, 400)
+
+        layout = QVBoxLayout()
+
+        self.text_area = QTextEdit()
+        self.text_area.setReadOnly(True)
+        self.text_area.setText(
+
+        )
+
+        layout.addWidget(self.text_area)
+        self.setLayout(layout)
