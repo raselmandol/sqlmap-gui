474f65fca2920c8460ec4b57d26fc4a0382d1fa1
raselmandol <raselmandol@yahoo.com>
2026-01-02 21:46:20 +0600

    pypi package name: sqlmap-gui

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sqlmap_gui',
+    name='sqlmap-gui',
     version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
